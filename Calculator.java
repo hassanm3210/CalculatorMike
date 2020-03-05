1f041ee28cae979432141324abec3ca36d801b6b
mawh <mawh@root.com>
2020-03-05 09:30:08 +0000

    Second version with few modification2

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,34 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Calculator {
 	public void SquareFeet() {
 		Scanner scan_length = new Scanner(System.in);
-		System.out.println("Please enter the length of the room in feet to calculate the area in square feet:");
+		System.out.println("Please enter the length in feet (in numeric format) to calculate the area in square feet:");
 		double length = scan_length.nextDouble();
 
 		Scanner scan_width = new Scanner(System.in);
-		System.out.println("Please enter the width of the room in feet to calculate the area in square feet:");
+		System.out.println("Please enter the width in feet (in numeric format) to calculate the area in square feet:");
 
 		double width = scan_width.nextDouble();
-		double s_feet = length * width;
-		System.out.println("The area in square feet is:" + s_feet + "SQAURE FEET");
+		DecimalFormat df = new DecimalFormat("###.##");
+		double s_feet = (length * width);
+		System.out.println("The area in square feet is:" + df.format(s_feet) + " SQAURE FEET");
 
 	}
 
 	public void SquareMeter() {
 		Scanner scan_length = new Scanner(System.in);
-		System.out.println("Please enter the length of the room in feet to calculate the area in square meter:");
+		System.out.println("Please enter the length in feet (in numeric format) to calculate the area in square meter:");
 		double length = scan_length.nextDouble();
 
 		Scanner scan_width = new Scanner(System.in);
-		System.out.println("Please enter the width of the room in feet to calculate the area in square meter:");
+		System.out.println("Please enter the width in feet (in numeric format) to calculate the area in square meter:");
 		double width = scan_width.nextDouble();
-
+		DecimalFormat df = new DecimalFormat("###.##");
 		double s_feet = length * width;
-		double s_meter = s_feet * 0.09290304;
-		System.out.println("The area in square feet is:" + s_meter + "SQUARE METER");
+		double s_meter = (s_feet * 0.09290304);
+		System.out.println("The area in square feet is:" + df.format(s_meter) + " SQUARE METER");
 
 	}
 
